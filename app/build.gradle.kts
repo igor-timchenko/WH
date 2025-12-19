dc7e348ee12896dde62c85f164e8647375e6b507
Владимир Кунжин <v.kunzhin@contlog.ru>
2025-12-19 15:08:57 +0700

    fix_1.0.2: 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "ru.contlog.mobile.helper"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
